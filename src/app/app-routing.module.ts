5f526fb3ec5dbb1c2b7f83962cc3141b67ae2d29
ibengwe <khalfankhamis81@gmail.com>
2024-05-17 18:03:28 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AdminlteComponent } from './components/layout/adminlte/adminlte.component';
+import { Dashboard1Component } from './components/pages/dashboard1/dashboard1.component';
+import { Dashboard2Component } from './components/pages/dashboard2/dashboard2.component';
+import { Dashboard3Component } from './components/pages/dashboard3/dashboard3.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:"",component:AdminlteComponent,children:[
+    {path:"",component:Dashboard1Component},
+    {path:"dashboard1",component:Dashboard1Component},
+    {path:"dashboard2",component:Dashboard2Component},
+    {path:"dashboard3",component:Dashboard3Component},
+
+  ]}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,37 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { AdminlteComponent } from './components/layout/adminlte/adminlte.component';
+import { MainHeaderComponent } from './components/layout/main-header/main-header.component';
+import { MainSidebarComponent } from './components/layout/main-sidebar/main-sidebar.component';
+import { ContentWrapperComponent } from './components/layout/content-wrapper/content-wrapper.component';
+import { ControlSidebarComponent } from './components/layout/control-sidebar/control-sidebar.component';
+import { MainFooterComponent } from './components/layout/main-footer/main-footer.component';
+import { Dashboard1Component } from './components/pages/dashboard1/dashboard1.component';
+import { Dashboard2Component } from './components/pages/dashboard2/dashboard2.component';
+import { Dashboard3Component } from './components/pages/dashboard3/dashboard3.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AdminlteComponent,
+    MainHeaderComponent,
+    MainSidebarComponent,
+    ContentWrapperComponent,
+    ControlSidebarComponent,
+    MainFooterComponent,
+    Dashboard1Component,
+    Dashboard2Component,
+    Dashboard3Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [
+    provideAnimationsAsync()
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
